5bb6cd6028c3ee999fdabcec967955d3f702de77
Margaux Mansanarez <margaux.mansanarez@gmail.com>
2021-04-16 16:47:38 +0200

    Added dependent restrict to Ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
 class Ingredient < ApplicationRecord
-  has_many :doses
+  has_many :doses, dependent: :restrict_with_error
   has_many :cocktails, through: :doses
 
   validates :name, presence: true, uniqueness: true
